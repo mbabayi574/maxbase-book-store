4160a1238a2052a4a2394dbf5a0855c6e6bf3a5c
Mohammadreza Babayi <m.babayi574@gmail.com>
2025-06-09 16:45:58 +0330

    feat: update Welcome component and remove unused assets

---
+++ app/pages/welcome/welcome.tsx
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <h1 className="text-6xl">سلام علیکم</h1>
+    </main>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../pages/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "خانه" },
+    { name: "description", content: "به صفحه اصلی خوش آمدید!" },
   ];
 }
 
